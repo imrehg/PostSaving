8f2d615b64ac6ec85f6cd2e7afd937bb1a736740
Gergely Imreh <imrehg@gmail.com>
2013-08-15 22:16:51 +0800

    Add comment and non-breaking space to postcard wall link

--- src/postsave.js
+++ src/postsave.js
@@ -1,11 +1,12 @@
 console.log("Arrived to postcrossing!");
 $(document).ready(function() {
+    // Postcards wall
     var postcards = $('#postcardImageList');
     postcards.find('li.postcard').each(function() {
 	var card = $(this);
 	var description = card.find('div');
 	var link = card.find('a');
-	description.append("<br><small><a href=\""+link.attr('href')+"\" download>Save image</a></small>");
+	description.append("<br><small><a href=\""+link.attr('href')+"\" download>Save&nbsp;image</a></small>");
     });
 
     // Individual postcard
