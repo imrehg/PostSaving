a78b22155a223c8ef777b2a67d02064e6907ec87
Gergely Imreh <imrehg@gmail.com>
2013-08-15 22:15:58 +0800

    add support for individual postcard images

--- src/postsave.js
+++ src/postsave.js
@@ -7,4 +7,14 @@ $(document).ready(function() {
 	var link = card.find('a');
 	description.append("<br><small><a href=\""+link.attr('href')+"\" download>Save image</a></small>");
     });
+
+    // Individual postcard
+    var postcardControls = $('#postcardControls');
+    if ( postcardControls.length ) {
+	var image = postcardControls.parent().parent().parent().find('img');
+	if ( image.length ) {
+	    var src = image.attr('src')
+	    postcardControls.append('<span><a href="'+src+'" download>Save&nbsp;image</a></span>');
+	}
+    }
 });
